11f344f3079caf7a057e1030e28eaa645b27a3ef
krispad-dev <kristofer.padoan@icloud.com>
2022-02-05 11:58:18 +0100

    loginController refactor

--- app/src/controllers/authController.ts
+++ app/src/controllers/authController.ts
@@ -19,15 +19,30 @@ export async function loginUser(req: Request, res: Response) {
         }
 
         const jwtPayload = {
+            userId: userTryingToLogIn.user_id,
             username: userTryingToLogIn.user_name,
             email: userTryingToLogIn.email,
             role: userTryingToLogIn.role,
         };
-  
-        const authToken = jwt.sign(jwtPayload, process.env.SECRET as string, { expiresIn: '1h' })
 
-        return res.cookie('authToken', authToken, { sameSite: 'strict', httpOnly: true, secure: true, maxAge: 3600000 }).json({
+        const authToken = jwt.sign(
+            jwtPayload, 
+            process.env.SECRET as string, 
+            { expiresIn: '1h' 
+        })
 
+        return res
+
+        .cookie(
+            'authToken', 
+            authToken, { 
+                sameSite: 'strict', 
+                httpOnly: true, 
+                secure: true, 
+                maxAge: 3600000 
+            })
+
+        .json({
             success: true, message:
                 `You are logged in as ${userTryingToLogIn.role}`
         })
